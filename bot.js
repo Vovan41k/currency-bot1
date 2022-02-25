6faf17c31715ce723d18bee23e42656fda37d2ff
Vovan41k <fatuev_vladimir@mail.ru>
2022-02-25 21:08:26 +0300

    functions to & from completed

--- bot.js
+++ bot.js
@@ -1,40 +1,44 @@
 require('dotenv').config()
 const token = process.env.TOKEN
 const TelegramBot = require('node-telegram-bot-api');
-const { rurToUsd, usdToRur, convertTo } = require('./lib/currency')
+const {  convertTo, convertFrom, signs } = require('./lib/currency')
 
 const bot = new TelegramBot(token, { polling: true });
 
 
-bot.onText(/\/rur (.+)/, (msg, match) => {
+// bot.onText(/\/rur (.+)/, (msg, match) => {
 
-  const chatId = msg.chat.id;
-  const rubles = match[1]; // the captured "whatever"
-  const dollars = rurToUsd(rubles)
-  bot.sendMessage(chatId, dollars + '$');
-});
+//   const chatId = msg.chat.id;
+//   const rubles = match[1]; // the captured "whatever"
+//   const dollars = rurToUsd(rubles)
+//   bot.sendMessage(chatId, dollars + '$');
+// });
 
-bot.onText(/\/usd (.+)/, (msg, match) => {
+// bot.onText(/\/usd (.+)/, (msg, match) => {
 
 
-  const chatId = msg.chat.id;
-  const dollars = match[1]; // the captured "whatever"
-  const rubles = usdToRur(dollars)
-  bot.sendMessage(chatId, rubles + 'Р');
-});
+//   const chatId = msg.chat.id;
+//   const dollars = match[1]; // the captured "whatever"
+//   const rubles = usdToRur(dollars)
+//   bot.sendMessage(chatId, rubles + 'Р');
+// });
 
 bot.onText(/\/to (.+)/, (msg, match) => {
   const words = match[1].split(' ')
-  const currency = words[0]
+  const currency = words[0].toUpperCase()
   const count = words[1]
   const chatId = msg.chat.id;
   const res = convertTo(currency, count)
-  bot.sendMessage(chatId, res + currency);
+  bot.sendMessage(chatId, res + signs[currency]);
 });
 
 bot.onText(/\/from (.+)/, (msg, match) => {
   const words = match[1].split(' ')
-  //todo
+  const currency = words[0].toUpperCase()
+  const count = words[1]
+  const chatId = msg.chat.id;
+  const res = convertFrom(currency, count)
+  bot.sendMessage(chatId, res + signs.RUR);
 });
 
 // bot.on('message', (msg) => {
