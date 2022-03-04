dd3a4689e8cd4e1204bcbe126d2d86b8b4a38f1f
Vovan41k <fatuev_vladimir@mail.ru>
2022-03-04 20:02:27 +0300

    currency rates added

---
+++ bot2.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const token = process.env.TOKEN
+const TelegramBot = require('node-telegram-bot-api');
+const {  convertTo, convertFrom, signs } = require('./lib/currency')
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/to (.+)/, (msg, match) => {
+    const words = match[1].split(' ')
+    const currency = words[0].toUpperCase()
+    const count = words[1]
+    const chatId = msg.chat.id;
+    const res = convertTo(currency, count)
+    bot.sendMessage(chatId, res + signs[currency]);
+});
+
+bot.onText(/\/from (.+)/, (msg, match) => {
+    const words = match[1].split(' ')
+    const currency = words[0].toUpperCase()
+    const count = words[1]
+    const chatId = msg.chat.id;
+    const res = convertFrom(currency, count)
+    bot.sendMessage(chatId, res + signs.RUR);
+});
